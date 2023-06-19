222ccb86559a7ae128a43af01865ed029e42d8f6
Draxus458P <4.ryanedwardamador@gmail.com>
2023-06-20 02:21:26 +0800

    Role based Login Secured sir

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:lunan/Patient/SignIn/log_in.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
@@ -10,7 +13,6 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) => const MaterialApp(
-    home: LoginPage(),
-  
-  );
+        home: LoginPage(),
+      );
 }
