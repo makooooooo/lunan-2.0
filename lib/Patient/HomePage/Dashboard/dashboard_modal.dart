f3a732c507077af214ed446bebc866215440b1eb
Draxus458P <4.ryanedwardamador@gmail.com>
2023-06-20 22:51:28 +0800

    Read na yung logged in user

--- lib/Patient/HomePage/Dashboard/dashboard_modal.dart
+++ lib/Patient/HomePage/Dashboard/dashboard_modal.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:lunan/Patient/HomePage/Dashboard/dashboard.dart';
 
 class DashboardModal extends StatelessWidget {
@@ -6,146 +8,163 @@ class DashboardModal extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder(
-      // future: _showDialog(context),
-      builder: (context, snapshot) => Scaffold(
-        backgroundColor: const Color(0xffF5E9CF), // Set the background color
-        appBar: AppBar(
-          automaticallyImplyLeading: false,
-          backgroundColor: const Color(0xff7DB9B6),
-          
-        ),
-        body: Center(
-          child: Container(
-            width: 300,
-            height: 250,
-            decoration: BoxDecoration(
-              color: const Color(0xff7DB9B6),
-              borderRadius: BorderRadius.circular(20),
-            ),
-            child: Column(
-              children: <Widget>[
-                Container(
-                  margin: const EdgeInsets.fromLTRB(0, 30, 0, 20),
-                  child: const Text(
-                    'Welcome back user!',
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                      fontFamily: 'Montserrat',
-                      fontSize: 25,
-                      color: Color(0xffffffff),
-                    ),
-                  ),
-                ),
-                Container(
-                  margin: const EdgeInsets.fromLTRB(0, 10, 0, 10),
-                  child: const Text(
-                    'How are you feeling\ntoday?',
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                      fontFamily: 'Montserrat',
-                      fontSize: 15,
-                      color: Color(0xffffffff),
-                    ),
-                  ),
-                ),
+    final user = FirebaseAuth.instance.currentUser;
+    final userId = user?.uid;
 
-                //emoji buttons
-                Row(
+    return StreamBuilder<QuerySnapshot>(
+      stream: FirebaseFirestore.instance
+          .collection("Users")
+          .where("UID", isEqualTo: userId)
+          .snapshots(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return CircularProgressIndicator();
+        } else if (snapshot.hasError) {
+          return Text('Error: ${snapshot.error}');
+        } else {
+          final documents = snapshot.data!.docs;
+          if (documents.isEmpty) {
+            return Text('User not found');
+          }
+
+          final firstName = documents[0].get('firstName') as String? ?? 'Unknown';
+
+          return Scaffold(
+            backgroundColor: const Color(0xffF5E9CF),
+            appBar: AppBar(
+              automaticallyImplyLeading: false,
+              backgroundColor: const Color(0xff7DB9B6),
+            ),
+            body: Center(
+              child: Container(
+                width: 300,
+            height: 325,
+                decoration: BoxDecoration(
+                  color: const Color(0xff7DB9B6),
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                child: Column(
                   mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    ElevatedButton(
-                      onPressed: () {
-                         Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => const Dashboard()),
-                      );
-                      
-                      },
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: Colors.white,
-                        shape: const CircleBorder(),
-                      ),
-                      child: const Text(
-                        '😡',
+                  children: <Widget>[
+                    Container(
+                      margin: const EdgeInsets.fromLTRB(0, 30, 0, 20),
+                      child: Text(
+                        'Welcome back $firstName!',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          fontFamily: 'Montserrat',
+                          fontSize: 25,
+                          color: Color(0xffffffff),
+                        ),
                       ),
                     ),
-                    ElevatedButton(
-                      onPressed: () {},
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: Colors.white,
-                        shape: const CircleBorder(),
-                      ),
-                      child: const Text(
-                        '😥',
-                      ),
-                    ),
-                    ElevatedButton(
-                      onPressed: () {},
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: Colors.white,
-                        shape: const CircleBorder(),
-                      ),
-                      child: const Text(
-                        '😔',
+                    const SizedBox(height: 10),
+                    const Text(
+                      'How are you feeling\ntoday?',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontFamily: 'Montserrat',
+                        fontSize: 15,
+                        color: Color(0xffffffff),
                       ),
                     ),
-                    ElevatedButton(
-                      onPressed: () {
-                      
-                      },
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: Colors.white,
-                        shape: const CircleBorder(),
-                      ),
-                      child: const Text(
-                        '😐',
-                      ),
+                    const SizedBox(height: 20),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        ElevatedButton(
+                          onPressed: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => const Dashboard(),
+                              ),
+                            );
+                          },
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: Colors.white,
+                            shape: const CircleBorder(),
+                          ),
+                          child: const Text(
+                            '😡',
+                          ),
+                        ),
+                        ElevatedButton(
+                          onPressed: () {},
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: Colors.white,
+                            shape: const CircleBorder(),
+                          ),
+                          child: const Text(
+                            '😥',
+                          ),
+                        ),
+                        ElevatedButton(
+                          onPressed: () {},
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: Colors.white,
+                            shape: const CircleBorder(),
+                          ),
+                          child: const Text(
+                            '😔',
+                          ),
+                        ),
+                        ElevatedButton(
+                          onPressed: () {},
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: Colors.white,
+                            shape: const CircleBorder(),
+                          ),
+                          child: const Text(
+                            '😐',
+                          ),
+                        ),
+                      ],
                     ),
+                    const SizedBox(height: 20),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        ElevatedButton(
+                          onPressed: () {},
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: Colors.white,
+                            shape: CircleBorder(),
+                          ),
+                          child: const Text(
+                            '🙂',
+                          ),
+                        ),
+                        ElevatedButton(
+                          onPressed: () {},
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: Colors.white,
+                            shape: CircleBorder(),
+                          ),
+                          child: const Text(
+                            '😊',
+                          ),
+                        ),
+                        ElevatedButton(
+                          onPressed: () {},
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: Colors.white,
+                            shape: CircleBorder(),
+                          ),
+                          child: const Text(
+                            '🤗',
+                          ),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(height: 20),
                   ],
                 ),
-
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    ElevatedButton(
-                      onPressed: () {},
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: Colors.white,
-                        shape: CircleBorder(),
-                      ),
-                      child: const Text(
-                        '🙂',
-                      ),
-                    ),
-                    ElevatedButton(
-                      onPressed: () {},
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: Colors.white,
-                        shape: CircleBorder(),
-                      ),
-                      child: const Text(
-                        '😊',
-                      ),
-                    ),
-                    ElevatedButton(
-                      onPressed: () {},
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: Colors.white,
-                        shape: CircleBorder(),
-                      ),
-                      child: const Text(
-                        '🤗',
-                      ),
-                    ),
-                  ],
-                )
-              ],
+              ),
             ),
-          ),
-        ),
-      ),
+          );
+        }
+      },
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:lunan/Patient/SignIn/log_in.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:lunan/Patient/Homepage/Dashboard/dashboard_modal.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -12,7 +13,12 @@ class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) => const MaterialApp(
-        home: LoginPage(),
-      );
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: LoginPage(),
+      routes: {
+        '/weeklyFormsQuestion': (context) => DashboardModal(),
+      },
+    );
+  }
 }
